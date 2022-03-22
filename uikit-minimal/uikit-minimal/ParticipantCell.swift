e76a95def74a0666e37078c6c5a73ec0be98f0ac
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-22 22:04:16 +0900

    minimal uikit ver

---
+++ uikit-minimal/uikit-minimal/ParticipantCell.swift
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2022 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import LiveKit
+
+class ParticipantCell: UICollectionViewCell {
+
+    public static let reuseIdentifier: String = "ParticipantCell"
+
+    public let videoView: VideoView = {
+        let r = VideoView()
+        r.layoutMode = .fit
+        r.backgroundColor = .black
+        return r
+    }()
+
+    public let labelView: UILabel = {
+        let r = UILabel()
+        r.textColor = .white
+        return r
+    }()
+
+    // weak reference to the Participant
+    public private(set) weak var participant: Participant?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("\(String(describing: self)) init")
+
+        backgroundColor = .brown
+
+        contentView.addSubview(videoView)
+        contentView.addSubview(labelView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        print("\(String(describing: self)) deinit")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        videoView.frame = contentView.bounds
+
+        labelView.sizeToFit()
+        labelView.frame = CGRect(x: (contentView.bounds.width - labelView.bounds.width) / 2,
+                                 y: contentView.bounds.height - labelView.bounds.height,
+                                 width: labelView.bounds.width,
+                                 height: labelView.bounds.height)
+    }
+
+    public func set(participant: Participant) {
+        guard self.participant != participant else { return }
+
+        if let previousParticipant = self.participant {
+            // un-listen previous participant's events
+            // in case this cell gets reused.
+            previousParticipant.remove(delegate: self)
+        }
+
+        // keep reference to current participant
+        self.participant = participant
+
+        // listen for events
+        participant.add(delegate: self)
+
+        labelView.text = participant.identity
+
+        setFirstVideoTrack()
+
+        // make sure the cell will call layoutSubviews()
+        setNeedsLayout()
+    }
+
+    private func setFirstVideoTrack() {
+        if let track = participant?.videoTracks.first?.track as? VideoTrack {
+            print("set track")
+            DispatchQueue.main.async {
+                self.videoView.track = track
+            }
+        }
+    }
+}
+
+extension ParticipantCell: ParticipantDelegate {
+
+    func participant(_ participant: RemoteParticipant, didSubscribe publication: RemoteTrackPublication, track: Track) {
+        print("didSubscribe")
+        setFirstVideoTrack()
+    }
+
+    func participant(_ participant: RemoteParticipant, didUnsubscribe publication: RemoteTrackPublication, track: Track) {
+        print("didUnsubscribe")
+        setFirstVideoTrack()
+    }
+}
---
+++ uikit-minimal/uikit-minimal/RoomViewController.swift
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2022 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import LiveKit
+
+// enter your own LiveKit server url and token
+let url = "ws://localhost:7880"
+let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDg2MTAyOTgsImlzcyI6IkFQSTdSTVB5ZWdUN29lSCIsImp0aSI6InVzZXIxIiwibmJmIjoxNjQ2MDE4Mjk4LCJzdWIiOiJ1c2VyMSIsInZpZGVvIjp7InJvb20iOiJyb29tMSIsInJvb21Kb2luIjp0cnVlfX0.2PYh9r8Oa9p1ESdllY3v2TigebJQdp69T_0dk8tl2mE"
+
+class RoomViewController: UIViewController {
+
+    lazy var room = Room(delegate: self)
+
+    lazy var collectionView: UICollectionView = {
+        print("creating UICollectionView...")
+        let r = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        r.backgroundColor = .blue
+        r.register(ParticipantCell.self, forCellWithReuseIdentifier: ParticipantCell.reuseIdentifier)
+        r.delegate = self
+        r.dataSource = room
+        r.alwaysBounceVertical = true
+        r.contentInsetAdjustmentBehavior = .never
+        return r
+    }()
+
+    override func loadView() {
+        print("loadView...")
+        super.loadView()
+        view.addSubview(collectionView)
+    }
+
+    override func viewWillLayoutSubviews() {
+        print("viewWillLayoutSubviews...")
+        super.viewWillLayoutSubviews()
+        collectionView.frame = view.bounds
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("connecting...")
+        room.connect(url, token).then { [weak self] room in
+            guard let self = self else { return }
+            print("connected to server version: \(String(describing: room.serverVersion))")
+
+            self.setParticipants()
+
+        }.catch { error in
+            print("failed to connect with error: \(error)")
+        }
+    }
+
+    private func setParticipants() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+}
+
+extension RoomViewController: RoomDelegate {
+
+    func room(_ room: Room, participantDidJoin participant: RemoteParticipant) {
+        setParticipants()
+    }
+
+    func room(_ room: Room, participantDidLeave participant: RemoteParticipant) {
+        setParticipants()
+    }
+}
+
+extension RoomViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        print("sizeForItemAt...")
+
+        let columns: CGFloat = 2
+        let size = collectionView.bounds.width / columns
+        return CGSize(width: size, height: size)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        .zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+}
+
+extension Room: UICollectionViewDataSource {
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // total number of participants to show (including local participant)
+        print("numberOfItemsInSection...")
+        return remoteParticipants.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        print("cellForItemAt...")
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ParticipantCell.reuseIdentifier,
+                                                      for: indexPath)
+
+        if let cell = cell as? ParticipantCell {
+
+            if indexPath.row < remoteParticipants.count {
+                let participant = Array(remoteParticipants.values)[indexPath.row]
+                cell.set(participant: participant)
+            }
+        }
+
+        return cell
+    }
+}
