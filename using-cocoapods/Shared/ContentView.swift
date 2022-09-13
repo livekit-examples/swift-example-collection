c3ace826051277159a0df795ee1cfbd41a999a41
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-09-13 16:33:55 +0900

    cocoapods example

---
+++ using-cocoapods/Shared/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Hiroshi Horie on 2022/09/13.
+//
+
+import SwiftUI
+import LiveKitClient
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("LiveKit Client SDK Ver. \(LiveKit.version)")
+            .padding()
+    }
+}
---
+++ using-cocoapods/Shared/using_cocoapodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  using_cocoapodsApp.swift
+//  Shared
+//
+//  Created by Hiroshi Horie on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct using_cocoapodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
