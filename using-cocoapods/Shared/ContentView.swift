a26383fab258b9755691add6885903f5ec326274
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-04-01 19:45:47 +0800

    update cocoapods example

--- using-cocoapods/Shared/ContentView.swift
+++ using-cocoapods/Shared/ContentView.swift
@@ -1,15 +1,23 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Hiroshi Horie on 2022/09/13.
-//
+/*
+ * Copyright 2024 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import SwiftUI
 import LiveKitClient
+import SwiftUI
 
 class Controller: ObservableObject {
-
     let room: Room
 
     init() {
@@ -17,8 +25,10 @@ class Controller: ObservableObject {
         room = Room()
         room.add(delegate: self)
 
-        room.connect("wss://_____.livekit.cloud",
-                     "")
+        Task {
+            try await room.connect(url: "wss://_____.livekit.cloud",
+                                   token: "")
+        }
     }
 
     deinit {
@@ -27,19 +37,16 @@ class Controller: ObservableObject {
 }
 
 extension Controller: RoomDelegate {
-
-    func room(_ room: Room, didUpdate connectionState: ConnectionState, oldValue: ConnectionState) {
-        //
+    func room(_: Room, didUpdateConnectionState connectionState: ConnectionState, from _: ConnectionState) {
         print("connectionState -> \(connectionState)")
     }
 }
 
 struct ContentView: View {
-
     @ObservedObject var ctrl = Controller()
 
     var body: some View {
-        Text("LiveKit Client SDK Ver. \(LiveKit.version)")
+        Text("LiveKit Client SDK Ver. \(LiveKitSDK.version)")
             .padding()
     }
 }
