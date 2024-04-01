e9dbe5c05a7024f25edce03560aed29e8709da07
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-04-02 02:19:42 +0800

    impl

--- custom-rendering/custom-rendering/ContentView.swift
+++ custom-rendering/custom-rendering/ContentView.swift
@@ -14,30 +14,85 @@
  * limitations under the License.
  */
 
+import AVFoundation
 import LiveKit
 import SwiftUI
 
-class MyRenderer: ObservableObject {
-    let track: LocalVideoTrack
-    init() {
-        print("init")
-        track = LocalVideoTrack.createCameraTrack()
+class MyCustomRendererView: NativeView {
+    public let sampleBufferDisplayLayer: AVSampleBufferDisplayLayer
+
+    override init(frame: CGRect) {
+        sampleBufferDisplayLayer = AVSampleBufferDisplayLayer()
+        super.init(frame: frame)
+        sampleBufferDisplayLayer.videoGravity = .resizeAspectFill
+        #if os(macOS)
+        // this is required for macOS
+        wantsLayer = true
+        layer?.insertSublayer(sampleBufferDisplayLayer, at: 0)
+        #elseif os(iOS)
+        layer.insertSublayer(sampleBufferDisplayLayer, at: 0)
+        #else
+        fatalError("Unimplemented")
+        #endif
     }
 
-    deinit {
-        print("deinit")
+    override func performLayout() {
+        super.performLayout()
+        sampleBufferDisplayLayer.frame = bounds
+        sampleBufferDisplayLayer.removeAllAnimations()
     }
 }
 
-struct CustomRenderView: View {
-    @ObservedObject var s = MyRenderer()
+// Conform to VideoRenderer
+extension MyCustomRendererView: VideoRenderer {
+    var isAdaptiveStreamEnabled: Bool { true }
+    var adaptiveStreamSize: CGSize { bounds.size }
+    func set(size _: CGSize) {}
+
+    func render(frame: LiveKit.VideoFrame) {
+        if let sampleBuffer = frame.toCMSampleBuffer() {
+            sampleBufferDisplayLayer.sampleBufferRenderer.enqueue(sampleBuffer)
+        }
+    }
+}
+
+// Make custom renderer view usable in SwiftUI
+struct MySwiftUICustomRendererView: NativeViewRepresentable {
+    let track: LocalVideoTrack
+
+    func makeView(context: Context) -> MyCustomRendererView {
+        let view = MyCustomRendererView()
+        updateView(view, context: context)
+        return view
+    }
+
+    func updateView(_ view: MyCustomRendererView, context _: Context) {
+        track.add(videoRenderer: view)
+    }
+
+    static func dismantleView(_: MyCustomRendererView, coordinator _: ()) {}
+}
+
+struct MyLocalVideoTrackView: View {
+    let track = LocalVideoTrack.createCameraTrack()
+
     var body: some View {
-        Text("Custom view")
+        MySwiftUICustomRendererView(track: track)
+            .onAppear(perform: {
+                Task {
+                    try await track.start()
+                }
+            })
+            .onDisappear(perform: {
+                Task {
+                    try await track.stop()
+                }
+            })
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        CustomRenderView()
+        MyLocalVideoTrackView()
     }
 }
